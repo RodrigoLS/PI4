31a2fbadfe230fc4692cff427ba59755d8b448d9
Julio Klein <julio.ckcalista@gmail.com>
2018-10-09 22:02:33 -0300

    first commit of julio monster

---
+++ RedeSocial/src/main/java/com/example/marcelobhegedus/pi4/RedeSocial/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.marcelobhegedus.pi4.RedeSocial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/marcelobhegedus/pi4/app/CadastroActivity.java
@@ -0,0 +1,13 @@
+package com.example.marcelobhegedus.pi4.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
