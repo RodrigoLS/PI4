eddd6addf0973be38a1bbf216b84f8e9f94d59b2
Julio Klein <julio.ckcalista@gmail.com>
2018-10-09 22:10:27 -0300

    julio

--- app/src/main/java/com/example/marcelobhegedus/pi4/app/CadastroActivity2.java
+++ app/src/main/java/com/example/marcelobhegedus/pi4/app/CadastroActivity2.java
@@ -9,6 +9,5 @@ public class CadastroActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_cadastro2);
     }
 }
