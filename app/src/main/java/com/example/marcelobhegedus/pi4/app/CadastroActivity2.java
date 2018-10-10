8db68288241acceb07b238c80a84a6108c64d2b0
RodrigoLS <rolima13@gmail.com>
2018-10-09 21:29:06 -0300

    cadastro layout

--- app/src/main/java/com/example/marcelobhegedus/pi4/app/CadastroActivity2.java
+++ app/src/main/java/com/example/marcelobhegedus/pi4/app/CadastroActivity2.java
@@ -2,16 +2,13 @@ package com.example.marcelobhegedus.pi4.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
-public class CadastroActivity extends AppCompatActivity {
+public class CadastroActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // getSupportActionBar().hide();
-       // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_cadastro);
-
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_cadastro2);
     }
 }
